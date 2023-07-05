386066e933fd8608c5ec898234426552d4c8f0b8
bruffin1221 <bruffin1221@gmail.com>
2023-07-04 18:45:31 -0700

    third-commit

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const lo = require('lodash')
 const numlist = require('./module-practice')
 console.log(numlist)
+console.log('person')
 
-const items = [1, [2, [3, 5]]]
-const newItems = lo.flattenDeep(items)
-console.log(newItems)
 
